95294989e534182745a791730d82f75b4dbd5753
LukaVieira1 <lukavieira14@gmail.com>
2025-01-08 06:57:09 -0300

    refactor: change name size

--- src/App.tsx
+++ src/App.tsx
@@ -47,7 +47,7 @@ function App() {
             </motion.div>
             <div className="w-full md:w-3/5">
               <motion.span
-                className="block text-primary-100 text-xl mb-4"
+                className="block text-primary-100 text-xl md:text-3xl mb-4"
                 initial={{ opacity: 0, y: 30 }}
                 animate={{ opacity: 1, y: 0 }}
                 transition={{ duration: 0.8 }}
